c866f710380616f4f9c0cdaa68ce377b1bfac2cd
crazyvan <crazyvan25@gmail.com>
2023-03-06 14:46:34 +0000

    blogrenderer

---
+++ blogrenderer/blogrenderer.go
@@ -0,0 +1,49 @@
+package blogrenderer
+
+import (
+	"embed"
+	"html/template"
+	"io"
+	"strings"
+)
+
+var (
+	//go:embed "templates/*.gohtml"
+	postTemplates embed.FS
+)
+
+type Post struct {
+	Title, Description, Body string
+	Tags                     []string
+}
+
+type PostViewModel struct {
+	Title, SanitisedTitle, Description, Body string
+	Tags                                     []string
+}
+
+type PostRenderer struct {
+	templ *template.Template
+}
+
+func (p Post) SanitisedTitle() string {
+	return strings.ToLower(strings.Replace(p.Title, " ", "-", -1))
+}
+
+func NewPostRenderer() (*PostRenderer, error) {
+	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &PostRenderer{templ: templ}, nil
+}
+
+func (r *PostRenderer) Render(w io.Writer, p Post) error {
+	return r.templ.ExecuteTemplate(w, "blog.gohtml", p)
+}
+
+func (r *PostRenderer) RenderIndex(w io.Writer, posts []Post) error {
+	return r.templ.ExecuteTemplate(w, "index.gohtml", posts)
+}
